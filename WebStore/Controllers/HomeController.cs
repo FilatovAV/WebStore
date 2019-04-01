129adfd29b839bb2c47e77176e1cb777a8e894cb
FilatovAV <a777f@mail.ru>
2019-04-01 23:24:38 +0300

    Подготовлены основные страницы и переходы между ними

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -9,6 +9,14 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
-
+        public IActionResult NotFound() => View();
+        public IActionResult Blog() => View();
+        public IActionResult BlogSingle() => View();
+        public IActionResult Cart() => View();
+        public IActionResult Checkout() => View();
+        public IActionResult ContactUs() => View();
+        public IActionResult Login() => View();
+        public IActionResult ProductDetails() => View();
+        public IActionResult Shop() => View();
     }
 }
