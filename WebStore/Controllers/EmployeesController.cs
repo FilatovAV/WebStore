5bba517e5392bfd1a73d494aa29ae8bfab69196b
FilatovAV <a777f@mail.ru>
2019-04-06 14:25:16 +0300

    IEmployeesData

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -10,33 +10,6 @@ namespace WebStore.Controllers
     public class EmployeesController : Controller
     {
 
-        private static List<Employee> employees = new List<Employee>()
-        {
-            new Employee
-            {
-                Id = 0,
-                FirstName = "Геннадий",
-                SurName = "Воржев",
-                Patronymic = "Степанович",
-                Age = 45
-            },
-            new Employee
-            {
-                Id = 1,
-                FirstName = "Алексей",
-                SurName = "Рахманин",
-                Patronymic = "Викторович",
-                Age = 45
-            },
-            new Employee
-            {
-                Id = 2,
-                FirstName = "Степан",
-                SurName = "Войтенков",
-                Patronymic = "Денисович",
-                Age = 45
-            }
-        };
         public IActionResult Index()
         {
             return View(employees);
---
+++ WebStore/Infrastructure/Interfaces/IEmployeesData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Controllers.Interfaces
+{
+    public interface IEmployeesData
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int id);
+        void AddNew(Employee employee);
+        void Delete(Employee employee);
+        void SaveChanges();
+    }
+}
