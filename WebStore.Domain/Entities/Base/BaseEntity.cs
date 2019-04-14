d134a2ecbcc3d4988a69fc6ae349f990abc926d8
FilatovAV <a777f@mail.ru>
2019-04-14 18:31:39 +0300

    Подключение EntityFrameworkCore.SqlServer

--- WebStore.Domain/Entities/Base/BaseEntity.cs
+++ WebStore.Domain/Entities/Base/BaseEntity.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace WebStore.Domain.Entities.Base
@@ -7,6 +9,7 @@ namespace WebStore.Domain.Entities.Base
     /// <summary>Сущность</summary>
     public abstract class BaseEntity : Interfaces.IBaseEntity
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 }
--- WebStore.Domain/Entities/Section.cs
+++ WebStore.Domain/Entities/Section.cs
@@ -1,9 +1,12 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities
 {
     /// <summary>Секция товаров</summary>
+    [Table("Sections")]
     public class Section : NamedEntity, IOrderedEntity
     {
         /// <summary>Порядок</summary>
@@ -11,5 +14,13 @@ namespace WebStore.Domain.Entities
 
         /// <summary>Идентификатор родительской секции</summary>
         public int? ParentId { get; set; }
+
+        [ForeignKey(nameof(ParentId))]
+        public virtual Section ParentSection { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+
+
+
     }
 }
