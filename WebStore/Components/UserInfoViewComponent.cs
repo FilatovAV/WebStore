5e3139a4b79ab8baff795363325d4fb9fd07c136
FilatovAV <a777f@mail.ru>
2019-06-01 17:22:12 +0300

    Система Identity

---
+++ WebStore/Components/UserInfoViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Components
+{
+    [ViewComponent(Name = "UserInfo")] //Можно задать имя компоненту
+    public class UserInfoViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View("UserInfoView");
+            }
+
+            return View();
+        }
+
+    }
+}
