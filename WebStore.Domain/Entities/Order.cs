82984ddb51d93ac3ad1c35cb36e1d45dc042db0c
FilatovAV <a777f@mail.ru>
2019-06-16 11:56:53 +0300

    Add-Migration add Order and Order Item

---
+++ WebStore.Domain/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entities.Base;
+
+namespace WebStore.Domain.Entities
+{
+    public class Order: NamedEntity
+    {
+        public virtual User User { get; set; }
+        public virtual string Phone { get; set; }
+        public DateTime Date { get; set; }
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+
+    }
+}
