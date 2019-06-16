220e23ff45a2def8c2b025515d1f5ff91d2b0770
FilatovAV <a777f@mail.ru>
2019-06-16 11:34:58 +0300

    - bootstrap 4.3.1 - admin area update - product list

--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin"), Authorize(Roles = "Administrator")]
     public class HomeController : Controller
     {
+        private readonly IProductData _productData;
+
+        public HomeController(IProductData productData)
+        {
+            this._productData = productData;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult ProductList()
+        {
+            return View(_productData.GetProducts(new ProductFilter()));
         }
     }
 }
