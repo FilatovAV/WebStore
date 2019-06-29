f8e2aa0128080c9c35fc5c409d8a4be7b59afc7c
FilatovAV <a777f@mail.ru>
2019-06-29 21:50:35 +0300

    Base client

---
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Net.Http.Headers;
+
+namespace WebStore.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        private readonly HttpClient _Client;
+        protected readonly string _ServiceAddress;
+        BaseClient(IConfiguration configuration, string serviceAddress)
+        {
+            _ServiceAddress = serviceAddress;
+
+            _Client = new HttpClient { BaseAddress = new Uri(configuration["ClientAddress"]) };
+            _Client.DefaultRequestHeaders.Accept.Clear();
+            //Указываем клиенту что бы он отправлял сервису заголовок что он ожидает обратно данные в формате json
+            _Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+    }
+}
