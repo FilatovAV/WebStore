cf8e12ba872f75ca4e14ead618a3f4dcf46ca939
FilatovAV <a777f@mail.ru>
2019-07-05 23:17:42 +0300

    Client Web API - IProductData

---
+++ Services/WebStore.Clients/Products/ProductsClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Domain.DTO.Product;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Clients.Products
+{
+    public class ProductsClient : BaseClient, IProductData
+    {
+        public ProductsClient(IConfiguration configuration) : base(configuration, "api/products")
+        {
+        }
+
+        public IEnumerable<Brand> GetBrands()
+        {
+            return Get<List<Brand>>($"{_ServiceAddress}/brands");
+        }
+
+        public ProductDTO GetProductById(int id)
+        {
+            return Get<ProductDTO>($"{_ServiceAddress}/{id}");
+        }
+
+        public IEnumerable<ProductDTO> GetProducts(ProductFilter filter)
+        {
+            var response = Post($"{_ServiceAddress}/products", filter);
+            return response.Content.ReadAsAsync<IEnumerable<ProductDTO>>().Result;
+        }
+
+        public IEnumerable<Section> GetSections()
+        {
+            return Get<List<Section>>($"{_ServiceAddress}/sections");
+        }
+    }
+}
