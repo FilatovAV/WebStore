2f3eef3acd42829df8efad3eddcfa47022444215
FilatovAV <a777f@mail.ru>
2019-07-09 23:38:23 +0300

    Добавление инфраструктуры для логирования log4net (начало)

---
+++ Common/WebStore.Logger/Log4NetLoggerProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Logger
+{
+    public class Log4NetLoggerProvider
+    {
+    }
+    public class Log4NetLogger
+    {
+    }
+
+    public static class Log4NetExtensions
+    {
+    }
+}
