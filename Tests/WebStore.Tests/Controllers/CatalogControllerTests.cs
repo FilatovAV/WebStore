f2b86d4473d2553679e43642d352deabd79200a8
FilatovAV <a777f@mail.ru>
2019-07-13 16:15:19 +0300

    + тест на метод Shop

--- Tests/WebStore.Tests/Controllers/CatalogControllerTests.cs
+++ Tests/WebStore.Tests/Controllers/CatalogControllerTests.cs
@@ -3,9 +3,11 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using WebStore.Controllers;
 using WebStore.Domain.DTO.Product;
+using WebStore.Domain.Entities;
 using WebStore.Domain.ViewModels.Product;
 using WebStore.Infrastructure.Interfaces;
 
@@ -107,6 +109,89 @@ namespace WebStore.Tests.Controllers
             //Проверяем что в результате мы получили NotFoundResult
             Assert.IsType<NotFoundResult>(result);
 
+            #endregion
+        }
+        [TestMethod]
+        public void Shop_Returns_Correct_View()
+        {
+            //Подготовка данных для тестирования и данных для проверки
+            #region Arrange
+
+            //Создаем фейковый элемент IProductData (макет интерфейса)
+            var product_data_mock = new Mock<IProductData>();
+            //Настраиваем фейковый объект, для этого передаем в метод GetProductById некое число It.IsAny<int>()
+            product_data_mock.
+                Setup(service => service.GetProducts(It.IsAny<ProductFilter>())).
+                Returns<ProductFilter>(filter => new[] 
+                {
+                    new ProductDTO()
+                    {
+                        Id = 1,
+                        Name = "Product 1",
+                        Order = 0,
+                        ImageUrl = "Product1.png",
+                        Price = 10m,
+                        Brand = new BrandDTO
+                        {
+                            Id = 1,
+                            Name = "Brand of Product 1"
+                        }
+                    },
+                    new ProductDTO()
+                    {
+                        Id = 2,
+                        Name = "Product 2",
+                        Order = 0,
+                        ImageUrl = "Product2.png",
+                        Price = 10m,
+                        Brand = new BrandDTO
+                        {
+                            Id = 1,
+                            Name = "Brand of Product 2"
+                        }
+                    },
+                    new ProductDTO()
+                    {
+                        Id = 3,
+                        Name = "Product 3",
+                        Order = 0,
+                        ImageUrl = "Product3.png",
+                        Price = 10m,
+                        Brand = new BrandDTO
+                        {
+                            Id = 1,
+                            Name = "Brand of Product 3"
+                        }
+                    }
+                });
+            //Передаем подготовленный объект контроллеру
+            _Controller = new CatalogController(product_data_mock.Object);
+
+            //Ожидаемые значения
+            const int expected_section_id = 1;
+            const int expected_brand_id = 5;
+
+            #endregion
+
+
+            //Процесс вызова тестируемого кода
+            #region Act
+            var result = _Controller.Shop(expected_section_id, expected_brand_id);
+
+            #endregion
+
+            //Проверка полученных результатов
+            #region Assert
+            //Проверяем что в результате мы получили представление
+            var view_result = Assert.IsType<ViewResult>(result);
+            //Извлекаем модель из представления
+            var model = Assert.IsAssignableFrom<CatalogViewModel>(view_result.ViewData.Model);
+
+            Assert.Equal(3, model.Products.Count());
+            Assert.Equal(expected_brand_id, model.BrandId);
+            Assert.Equal(expected_section_id, model.SectionId);
+
+            Assert.Equal("Brand of Product 1", model.Products.First().Brand);
             #endregion
         }
     }
